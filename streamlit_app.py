51f24535da638f9fc97f807bc15553d9902c3edd
mukGH123 <143869357+mukGH123@users.noreply.github.com>
2024-09-14 22:28:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,16 @@ def store_details():
     st.header(f'Employee: {store_name}')
     st.write('### Employee Details')
     st.write(f"Code:  {store_data['Code']}")
+     st.write(f"Total CTC:  {store_data['TotalCTC']}")
+ st.write(f"Total CTC:  {store_data['TotalCTC']}")
+ st.write(f"Total CTC:  {store_data['TotalCTC']}")
+ st.write(f"Total CTC:  {store_data['TotalCTC']}")
+ st.write(f"Total CTC:  {store_data['TotalCTC']}")
+ st.write(f"Total CTC:  {store_data['TotalCTC']}")
+ st.write(f"Other Allowance:  {store_data['OtherAllowance']}")
+ st.write(f"GROSS SALARY:  {store_data['GROSSSALARY']}")
+ st.write(f"Total CTC:  {store_data['TotalCTC']}")
+
     st.write(f"Total CTC:  {store_data['TotalCTC']}")
    
    # st.write(f"Code:  {store_data['Code']}")
