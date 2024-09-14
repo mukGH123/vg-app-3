0f86186efcaaf451f8426ac58cee5dc5ae949869
mukGH123 <143869357+mukGH123@users.noreply.github.com>
2024-09-14 22:30:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,7 @@ def store_details():
     # Display store attributes
     st.header(f'Employee: {store_name}')
     st.write('### Employee Details')
-     st.write(f"Total CTC:  {store_data['TotalCTC']}")
-    st.write(f"Code:  {store_data['Code']}")
+     st.write(f"Code:  {store_data['Code']}")
 
  
    
