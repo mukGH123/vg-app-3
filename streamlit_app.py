6d69949662ddec6c4bfbd29776e676089c66dbfe
mukGH123 <143869357+mukGH123@users.noreply.github.com>
2024-09-14 21:38:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,48 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Load the Excel file
+df = pd.read_excel('Revised-Data 25-08-24.xlsx')
+
+# Format Store IDs (name in your case) to avoid comma separation
+df['Name'] = df['Name'].astype(str)
+
+# Define pages
+def store_details():
+    st.sidebar.header('Select Employee Name')
+    store_name = st.sidebar.selectbox('Employee Name', df['Name'].unique())
+
+    # Filter data for the selected store
+    store_data = df[df['Name'] == store_name].iloc[0]
+
+    # Display store attributes
+    st.header(f'Employee: {store_name}')
+    st.write('### Employee Details')
+    st.write(f"Code: {store_data['Code']}")
+    
+
+   
+
+# Create a navigation menu
+st.sidebar.title('Navigation')
+page = st.sidebar.radio('Go to', ['Employee Details', 'Employee Statistics', 'Employee Comparison'])
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stElementToolbar"] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
+
+# Render the selected page
+if page == 'Employee Details':
+    store_details()
+elif page == 'Employee Statistics':
+    store_details()
+elif page == 'Employee Comparison':
+    store_details()
