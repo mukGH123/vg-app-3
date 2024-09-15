9450637c402fa8cd42e66f6260ab33e9b2238341
mukGH123 <143869357+mukGH123@users.noreply.github.com>
2024-09-15 10:26:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,18 @@ import pyodbc
 
 # Function to connect to SQL Server and fetch data
 def get_data_from_sql():
-    conn = pyodbc.connect(
-        'DRIVER={SQL Server};'
-        'SERVER=202.66.174.120,1232;'
-        'DATABASE=vaagmndb;'
-        'UID=vaagdadbusr;'
-        'PWD=MefrAyu!Uw8they9ru;'
-    )
+	conn = pyodbc.connect(
+		'DRIVER={SQL Server};'
+		'SERVER=202.66.174.120,1232;'
+		'DATABASE=vaagmndb;'
+		'UID=vaagdadbusr;'
+		'PWD=MefrAyu!Uw8they9ru;'
+	)
 	
-    query = "SELECT Employee_Name, Employee_Code, Employee_Gender, Employee_DOB, GrossSalary FROM EmployeeMater"
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
+	query = "SELECT Employee_Name, Employee_Code, Employee_Gender, Employee_DOB, GrossSalary FROM EmployeeMater"
+	df = pd.read_sql(query, conn)
+	conn.close()
+	return df
 
 # Load the data from SQL Server
 df = get_data_from_sql()
